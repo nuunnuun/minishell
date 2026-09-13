a1724eeb20225a934f87dba196a4d9d156d1019e
rookiethedev <rookiethedev23@gmail.com>
2026-09-13 21:25:00 +0200

    minishell latest build

---
+++ minishell_code/helpers/utils_strings2.c
@@ -0,0 +1,88 @@
+#include "minishell.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+	size_t	i;
+	size_t	j;
+	size_t	len;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		return (ft_strdup(s2));
+	if (s2 == NULL)
+		return (ft_strdup(s1));
+	len = ft_strlen(s1) + ft_strlen(s2);
+	join = malloc(len + 1);
+	if (join == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		join[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		join[i + j] = s2[j];
+		j++;
+	}
+	join[i + j] = '\0';
+	return (join);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+
+	if (s == NULL)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	sub = malloc(len + 1);
+	if (sub == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[len] = '\0';
+	return (sub);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*trim;
+	size_t	start;
+	size_t	end;
+	size_t	i;
+
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	if (s1[start] == '\0')
+		return (ft_strdup(""));
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	trim = malloc(end - start + 1);
+	if (trim == NULL)
+		return (NULL);
+	i = 0;
+	while (i < end - start)
+	{
+		trim[i] = s1[start + i];
+		i++;
+	}
+	trim[end - start] = '\0';
+	return (trim);
+}
